f526e51f566a277eb3e7a119dc83f29a2a6e76ee
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-28 18:49:17 +0800

    Add files via upload

--- GMT-27-various-Unix-progs.sh
+++ GMT-27-various-Unix-progs.sh
@@ -10,6 +10,7 @@ $0 — представляет всю строку текста (запись).
 $1 — первое поле.
 $2 — второе поле.
 $n — n-ное поле.
+# позиционные переменные — $1, $2, $3, которые позволяют извлекать значения полей
 # 4. выкусит долготы (1 столбец)
 awk '{print $1}' Ab.txt > Alon.txt
 # 5. выкусит широты (2 столбец)
@@ -18,6 +19,38 @@ awk '{print $2}' Ab.txt > Alat.txt
 $ echo "My name is Tom" | awk '{$4="Adam"; print $0}'
 # 7. выполнить какие-то действия до того, как скрипт начнёт обработку записей из входного потока. Например — создать шапку отчёта или что-то подобное.
 awk 'BEGIN {print "Hello World!"}'
-# блоки BEGIN и END
+# 8. блоки BEGIN и END
 awk 'BEGIN {print "The File Contents:"} {print $0}' Ab.txt
 $ awk 'BEGIN {print "The File Contents:"} {print $0} END {print "End of File"}' Ab.txt
+# 9. echo+awk
+echo 'one two three four' | awk '{print $1}'
+echo 'one two three four' | awk '{print $3,$1}'
+# 10. some Russian text
+echo 'one two three four' | awk '{print "что-то:",$3,"| bar:",$1}'
+# 11. separator : -F":"
+echo 'one mississippi:two mississippi:three mississippi:four mississippi' | awk -F":" '{print $4}'
+echo 'one mississippi:two mississippi:three mississippi:four mississippi' | awk -F: '{print $4}'
+# 12. последнее поле
+echo 'one two three four' | awk '{print $NF}'
+# 13. предпоследнее поле
+echo 'one two three four' | awk '{print $(NF-1)}'
+# 14. выбрать выражением
+echo 'one two three four' | awk '{print $((NF/2)+1)}'
+# 15. строка с новой строки
+echo -e 'one 1\n two 2' | awk '{print $1}'
+echo -e 'one 1\n two 2' | awk '{print $2}'
+echo -e 'one 1\n two 2' | awk '{sum+=$2} END {print sum}'
+# FS — переменная, позволяющая задавать символ-разделитель полей.
+# RS — переменная, которая позволяет задавать символ-разделитель записей.
+# OFS — разделитель полей на выводе awk-скрипта.
+# ORS — разделитель записей на выводе awk-скрипта.
+awk 'BEGIN{OFS=":"} {print $1,$2,$3}' Ab.txt > Abcolon.txt
+awk 'BEGIN{FS=":"; OFS="-"} {print $1,$2,$3}' Abcolon.txt > Abdefis.txt
+awk 'BEGIN{OFS="|"} {print $7}' GSFML_my.txt > GSFML_my_new.txt
+awk '{print $1}' GSFML_my.txt > GSFML_my_new.txt
+awk '{print $1}' GSFML_my_new.txt > GSFML_my_n.txt
+awk 'BEGIN{FIELDWIDTHS="8 1 1 3"}{print $1,$2,$3,$4}' GSFML_my.txt > GSFML_my_new.txt
+awk 'BEGIN{FIELDWIDTHS="3 5 2 5"}{print $1,$2,$3,$4}' awk_testfile.txt
+awk '{print $3}' awk_testfile.txt > GSFML_my_new3.txt
+#
+awk 'BEGIN{FS="\n"; RS=""} {print $1,$3}' awk_addresses.txt
